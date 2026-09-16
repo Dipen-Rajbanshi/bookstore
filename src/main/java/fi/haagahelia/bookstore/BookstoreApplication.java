a0169ff80c8abd07c49dae3d41ed175f16a45d6d
Dipen Rajbanshi <dipen.rajbanshi@myy.haaga-helia.fi>
2026-09-16 16:16:35 +0300

    Add JPA, H2, and booklist page

--- src/main/java/fi/haagahelia/bookstore/BookstoreApplication.java
+++ src/main/java/fi/haagahelia/bookstore/BookstoreApplication.java
@@ -1,13 +1,27 @@
 package fi.haagahelia.bookstore;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import fi.haagahelia.bookstore.domain.Book;
+import fi.haagahelia.bookstore.domain.BookRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner demo(BookRepository repository) {
+		return (args) -> {
+			repository.save(new Book("The Great Gatsby", "F. Scott Fitzgerald", 1925, "9780743273565", 10.99));
+			repository.save(new Book("To Kill a Mockingbird", "Harper Lee", 1960, "9780061120084", 7.99));
+			repository.save(new Book("1984", "George Orwell", 1949, "9780451524935", 8.99));
+		};
 	}
 
 }
--- src/main/java/fi/haagahelia/bookstore/web/BookController.java
+++ src/main/java/fi/haagahelia/bookstore/web/BookController.java
@@ -2,12 +2,27 @@ package fi.haagahelia.bookstore.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
+import fi.haagahelia.bookstore.domain.BookRepository;
 
 @Controller
 public class BookController {
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
 
     @GetMapping("/index")
     public String index() {
         return "index";
     }
+
+    @GetMapping("/booklist")
+    public String booklist(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
+        return "booklist";
+    }
 }
+
