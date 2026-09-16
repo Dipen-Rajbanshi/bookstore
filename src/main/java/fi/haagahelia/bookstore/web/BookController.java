55eef3115df4eceb8ac8fd9afc3bdc67c69996c4
Dipen Rajbanshi <dipen.rajbanshi@myy.haaga-helia.fi>
2026-09-16 16:41:57 +0300

    Add CRUD functionality: create, edit, delete books

--- src/main/java/fi/haagahelia/bookstore/web/BookController.java
+++ src/main/java/fi/haagahelia/bookstore/web/BookController.java
@@ -1,13 +1,18 @@
 package fi.haagahelia.bookstore.web;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
+import fi.haagahelia.bookstore.domain.Book;
 import fi.haagahelia.bookstore.domain.BookRepository;
 
 @Controller
 public class BookController {
+
     private final BookRepository bookRepository;
 
     public BookController(BookRepository bookRepository) {
@@ -24,5 +29,29 @@ public class BookController {
         model.addAttribute("books", bookRepository.findAll());
         return "booklist";
     }
-}
 
+    @GetMapping("/addbook")
+    public String addBookForm(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+
+    @PostMapping("/addbook")
+    public String addBook(@ModelAttribute Book book) {
+        bookRepository.save(book);
+        return "redirect:/booklist";
+    }
+
+    @GetMapping("/editbook/{id}")
+    public String editBookForm(@PathVariable Long id, Model model) {
+        Book book = bookRepository.findById(id).orElseThrow();
+        model.addAttribute("book", book);
+        return "addbook";
+    }
+
+    @GetMapping("/deletebook/{id}")
+    public String deleteBook(@PathVariable Long id) {
+        bookRepository.deleteById(id);
+        return "redirect:/booklist";
+    }
+}
